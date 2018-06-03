a3021dffa434ebe0b103f81e26461dd6c5a14302
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 21:20:38 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  # scope '/admin', module: 'admin' do
+  # NOTE : Replace "scope/module" with "namespace" when using both of the same
+    # If moving from "scope" to "namespace" -> Need to remember to update all URL helpers
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
+
   root 'posts#index'
 end
